3dd86c6c0418967a54d5be37b71786d3dbc0c3f5
Anil Varisetty <anil.varisetty@gmail.com>
2021-04-05 16:38:26 +0530

    Updated Main File V1.1

--- main.java
+++ main.java
@@ -9,7 +9,6 @@ class Simple
           // Added a comment line 01 in dev
           // Added a comment line 02 in dev branch
           // Added Comment line new 03 in dev
-          System.out.println("20 -5 is " +  substarctLib(20,5) );
      }
      public static int Add(int a, int b)
      {
