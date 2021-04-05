9b1588b592ecff2870c6baab0d8c69c441458c30
Anil Varisetty <anil.varisetty@gmail.com>
2021-04-05 15:25:43 +0530

    Updated V1.0

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 class Simple
 {
      public static void main(String args[])
@@ -7,5 +9,11 @@ class Simple
           // Added a comment line 01 in dev
           // Added a comment line 02 in dev branch
           // Added Comment line new 03 in dev
+          System.out.println("20 -5 is " +  substarctLib(20,5) );
+     }
+     public static int Add(int a, int b)
+     {
+          return a+b;
      }
 }
+
