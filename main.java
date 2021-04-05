ae663028a9cdef3fd8b85d6158c6a9bc677f9935
Anil Varisetty <anil.varisetty@gmail.com>
2021-04-05 14:57:33 +0530

    Modified main added new comment line

--- main.java
+++ main.java
@@ -3,7 +3,8 @@ class Simple
      public static void main(String args[])
      {
      	System.out.println("Hello Java");
-     // Added a comment line 01 in dev
+          // Added a comment line 01 in dev
           // Added a comment line 02 in dev branch
+          // Added Comment line new 03 in dev
      }
 }
